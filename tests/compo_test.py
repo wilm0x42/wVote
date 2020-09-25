eef21ca10029503304bc66a5e8dc33e08b535656
Metallion <akieckens@degica.com>
2020-09-25 15:28:17 +0900

    Added another test that makes sure we get a valid uuid

--- tests/compo_test.py
+++ tests/compo_test.py
@@ -1,5 +1,14 @@
 import compo
+import uuid
 
-def test_create_blank_entry():
+def test_create_blank_entry_returns_string():
     result = compo.create_blank_entry("wiglaf", discord_id="is a wiener")
     assert type(result) is str
+
+def test_create_blank_entry_returns_valid_uuid():
+    result = compo.create_blank_entry("wiglaf", discord_id="is still a wiener")
+
+    try:
+        uuid.UUID(result)
+    except ValueError:
+        pytest.fail("create_blank_entry did not return a valid uuid.")
