c8651a0cd89a3f82e61576eb448b2518e8adf364
Metallion <akieckens@degica.com>
2020-09-25 16:08:21 +0900

    Put the tests for create blank entry in a class

--- tests/compo_test.py
+++ tests/compo_test.py
@@ -1,14 +1,15 @@
 import compo
 import uuid
 
-def test_create_blank_entry_returns_string():
-    result = compo.create_blank_entry("wiglaf", discord_id="is a wiener")
-    assert type(result) is str
+class TestCreateBlankEntry:
+    def test_create_blank_entry_returns_string(self):
+        result = compo.create_blank_entry("wiglaf", discord_id="is a wiener")
+        assert type(result) is str
 
-def test_create_blank_entry_returns_valid_uuid():
-    result = compo.create_blank_entry("wiglaf", discord_id="is still a wiener")
+    def test_create_blank_entry_returns_valid_uuid(self):
+        result = compo.create_blank_entry("wiglaf", discord_id="is still a wiener")
 
-    try:
-        uuid.UUID(result)
-    except ValueError:
-        pytest.fail("create_blank_entry did not return a valid uuid.")
+        try:
+            uuid.UUID(result)
+        except ValueError:
+            pytest.fail("create_blank_entry did not return a valid uuid.")
