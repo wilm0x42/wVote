c51d785d5ba1ac4e1b100a887a9dadff4728881e
Guillermo O. «Tordek» Freschi <kedrot@gmail.com>
2020-11-03 16:26:58 -0300

    Move templates into a subdirectory
    Slight organization.

--- http_server.py
+++ http_server.py
@@ -11,10 +11,10 @@ from aiohttp import web, web_request
 import bot
 import compo
 
-vote_template = open("vote.html", "r").read()
-submit_template = open("submit.html", "r").read()
-submit_success = open("submitted.html", "r").read()
-admin_template = open("admin.html", "r").read()
+vote_template = open("templates/vote.html", "r").read()
+submit_template = open("templates/submit.html", "r").read()
+submit_success = open("templates/submitted.html", "r").read()
+admin_template = open("templates/admin.html", "r").read()
 
 favicon = open("static/favicon.ico", "rb").read()
 
