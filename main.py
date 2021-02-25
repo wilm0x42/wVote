858ebc848a84c187d73fd78a201e7cec7a10f79b
Guillermo O. «Tordek» Freschi <kedrot@gmail.com>
2021-02-25 20:03:26 -0300

    Assign the variables or it dies randomly.

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ bot.load_config()
 
 loop = asyncio.get_event_loop()
 
-loop.create_task(bot.client.start(bot.client.bot_key))
-loop.create_task(http_server.start_http())
+bot_task = loop.create_task(bot.client.start(bot.client.bot_key))
+http_task = loop.create_task(http_server.start_http())
 
 loop.run_forever()
