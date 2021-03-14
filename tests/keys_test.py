bf996caac740882a8e87ad0e5bf2b6293d05f406
Copeland Steele <admiral_gamer@outlook.com>
2021-03-14 16:55:29 -0500

    tests for key_valid

--- tests/keys_test.py
+++ tests/keys_test.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from string import ascii_letters, digits
 from uuid import uuid4
 import json
@@ -10,10 +10,50 @@ config = json.load(open("botconf.json", "r"))
 keys.configure(config)
 
 class TestKeyValid():
-    pass
+    # is it unneccessary to do all the tests with each type of key?
+    # probably... 
+                           # callable to create key, store 4 key, args 4 keygen
+    @pytest.fixture(params=[(keys.create_edit_key, keys.edit_keys, ("spam",)),
+                            (keys.create_admin_key, keys.admin_keys, ()),
+                            (keys.create_vote_key, keys.vote_keys, (42, "a"))])
+    def multi_run(self, request):
+        return request
+    
+    @pytest.fixture()
+    def valid_key(self, multi_run):
+        key = multi_run.param[0](*multi_run.param[2])
+        yield key, multi_run.param[1], multi_run.param[2]
+        del multi_run.param[1][key]
 
+    @pytest.fixture()
+    def expired_key(self, multi_run):
+        key_gen, store, args = multi_run.param
+        key = key_gen(*args)
+        full_key = store[key]
+        past_ttl = timedelta(minutes=full_key["timeToLive"] + 1)
+        full_key["creationTime"] -= past_ttl
+        return key, store, args
 
-# everything past here is maybe a little unnecessary but whatevs....
+    def test_key_valid(self, valid_key):
+        assert keys.key_valid(valid_key[0], valid_key[1])
+
+    def test_key_not_in_store(self, valid_key):
+        assert keys.key_valid(valid_key[0], {}) == False
+    
+    def test_key_expired(self, expired_key):
+        assert keys.key_valid(expired_key[0], expired_key[1]) == False
+
+    def test_expired_key_removed(self, expired_key):
+        key, store, args = expired_key
+        keys.key_valid(key, store)
+        assert key not in store
+
+    # everything past here is maybe a little unnecessary but whatevs....
+    def test_return_bool(self, valid_key, expired_key):
+        assert isinstance(keys.key_valid(valid_key[0], valid_key[1]), bool)
+        assert isinstance(keys.key_valid(valid_key[0], {}), bool)
+        assert isinstance(keys.key_valid(expired_key[0], expired_key[1]), bool)
+
 class TestCreateKey():
     def test_key_valid_chars(self):
         alphanumeric = ascii_letters + digits
@@ -32,7 +72,7 @@ class TestCreateKey():
             if len(key) != i:
                 passing = False
             assert passing
-
+    
     def test_return_str(self):
         assert isinstance(keys.create_key(8), str)
 
