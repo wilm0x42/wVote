98b9ebd9b31c7f33ca6eea209c45f6a5a9e4660e
set <guillermo.freschi@securitize.io>
2022-08-01 20:55:34 -0300

    Add dummy botconfig

---
+++ botconfig.py
@@ -0,0 +1,12 @@
+class config():
+    def __init__(self):
+        self.command_prefix = "8!"
+        self.bot_key = ""
+        self.notify_admins_channel = 0
+        self.url_prefix = "https://www.example.com/"
+        self.default_ttl = 900
+        self.deadline_time_offset = 0
+        self.deadline_weekday = 5
+        self.allowed_hosts = ["http://souncloud.com"]
+
+Config = config()
